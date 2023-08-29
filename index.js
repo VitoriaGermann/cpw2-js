30ecc28e01f5d5ec88a5191f96b3b7ceea2b931f
VitoriaGermann <68782248+VitoriaGermann@users.noreply.github.com>
2023-08-29 00:00:29 +0000

    TESTE

--- index.js
+++ index.js
@@ -1,68 +1,51 @@
-
 /**
  * Recebe um array com os nomes de pessoas e retorna um array invertido
  *
- * @param {*} people : Um array contendo um conjunto de nomes
+ * @param {Array} people : Um array contendo um conjunto de nomes
  * @returns Um array com os nomes invertidos
  */
-function invert(people){
-
-    // TODO
-    // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
-    // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
-    // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
-    // nomes dos usuários.
-
-    return [];
+function invert(people) {
+    return people.reverse();
 }
 
 /**
  * Essa função recebe um array de notas e retorna uma média
  *
- * @param {*} grades : Um array de notas
+ * @param {Array} grades : Um array de notas
  * @returns Uma média a partir do array de notas
  */
-function mean(grades){
-
-    // TODO
-    // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
-
-    return 0;
+function mean(grades) {
+    const sum = grades.reduce((total, grade) => total + grade, 0);
+    return sum / grades.length;
 }
 
 /**
  * Função recebe uma média e informa textualmente se um estudante está aprovado ou não
  *
- * @param {*} mean : Um número que representa uma média
+ * @param {Number} mean : Um número que representa uma média
  * @returns Uma string dizendo se um estudante está aprovado ou não
  */
-function isApproved(mean){
-
-    // TODO
-    // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
-
-    return "";
+function isApproved(mean) {
+    return mean >= 7 ? "Aprovado" : "Reprovado";
 }
 
 /**
  * Recebe uma data no formato de string e transforma em um formato textual
  *
- * @param {*} strDate : Uma string no formato de data
+ * @param {String} strDate : Uma string no formato de data (dd/mm/aaaa)
  * @returns Uma descrição da data informada
  */
-function wide(strDate){
+function wide(strDate) {
+    const months = [
+        "janeiro", "fevereiro", "março", "abril", "maio", "junho",
+        "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+    ];
 
-    // TODO
-    // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
-    // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
-    // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
-    // Nesse caso, o separador é a barra (/) da data.
+    const [day, month, year] = strDate.split("/");
+    const monthIndex = parseInt(month, 10) - 1;
+    const monthName = months[monthIndex];
 
-    return "";
+    return `${day} de ${monthName} de ${year}`;
 }
 
 // Trecho utilizado pelos testes
@@ -71,4 +54,4 @@ exports._test = {
     mean: mean,
     isApproved: isApproved,
     wide: wide
-}
+};
